712cabaea69f0ac18bf1c5b16fe2a6ae1ba7ef38
chhagan123 <chhaganrakhade7@gmail.com>
2025-12-28 21:01:39 +0530

    feat: added Arrow svg and implemented Branches component

---
+++ src/views/setup/Branches.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="w-full h-full flex flex-col justify-start gap-8">
+    <div class="flex flex-col gap-1">
+    <div class="text-md font-bold text-[#000000BF]">
+      BRANCHES
+    </div>
+    <div>
+        Add Branches and manage branch details like Teams and Positions from here
+    </div>
+    </div>
+    <div class="gap-2 flex items-center ">
+     <div class="bg-black/10 rounded-lg w-8 h-8 items-center justify-center flex">
+        <Arrow class="w-6 h-6"/>
+     </div>
+    <div class="text-xl font-bold text-[#000000BF]">
+      Actions
+    </div>
+    </div>
+</div>
+    
+
+</template>
+
+<script setup lang="ts">
+import Arrow from '../../assets/icons/Arrow/Arrow.vue';
+  
+</script>
