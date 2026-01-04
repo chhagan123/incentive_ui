3653b9bcd69d46fcdbe5bafd24f6af9dff299fa2
chhagan123 <chhaganrakhade7@gmail.com>
2026-01-04 15:36:25 +0530

    feat: added Home page component

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="w-full h-screen flex justify-center items-center">
+    <h1 class="text-2xl underline hover:bg-amber-600">hellow world</h1>
+  </div>
+
+</template>
+
+<style scoped>
+</style>
