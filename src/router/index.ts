fb887191c9ac543b8c76b0995f686052b50885af
chhagan123 <chhaganrakhade7@gmail.com>
2026-01-05 14:34:51 +0530

    feat: added breadcrumb metadata and new routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,13 +3,37 @@ import { createRouter,createWebHistory } from "vue-router";
 import Home from '../views/Home.vue'
 
 const routes = [
-    {path:'/',name:'Home',component:Home}
+    {
+      path: "/",
+      component: Home,
+      meta: { breadcrumb: "" },
+      children: [
+        {
+          path: "overview",
+          name: "Overview",
+          component: Home,
+          meta: { breadcrumb: "Overview" },
+        },
+        {
+            path: "employees",
+            name: "employess",
+            component: Home,
+            meta: { breadcrumb: "employees" },
+          },
+        {
+            path: "calculator",
+            name: "Calculator",
+            component: Home,
+            meta: { breadcrumb: "Calculator" },
+          }
+      ]
+    }
+  ]
 
-]
-
-const router = createRouter({
-    history:createWebHistory(),
+  const router = createRouter({
+    history: createWebHistory(),
     routes
-})
-
+  })
+  
+  
 export default router
