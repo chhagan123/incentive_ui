59625a95f06efe710dabb061dab873bbc2cb0d58
chhagan123 <chhaganrakhade7@gmail.com>
2026-01-05 11:30:46 +0530

    feat: integrate Navbar into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <RouterView />
+  <div class="min-h-screen flex flex-col">
+    <Navbar />
+    <!-- Main Content -->
+    <main class="flex-1">
+      <div class="max-w-7xl border-3 border-amber-100 mx-auto px-4 py-8">
+        <RouterView />
+      </div>
+    </main>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import Navbar from "./components/Navbar.vue";
 
 </script>
 
