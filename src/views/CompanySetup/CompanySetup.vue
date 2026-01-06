174c465b0892722f4d5fb307ae58f58e8bec8e3d
chhagan123 <chhaganrakhade7@gmail.com>
2026-01-06 12:54:12 +0530

    feat: added CompanySetup page component

---
+++ src/views/CompanySetup/CompanySetup.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="flex flex-col gap-6 ">
+    <div>
+      <h1 class="text-4xl text-black/75 text-opacity-75 font-medium">
+        Company setup
+      </h1>
+    </div>
+    <p class="text-base text-gray-400">
+      To start using Incentive Engine, first set up your company. Add a branch,
+      add positions, and create a payout template
+    </p>
+    <div
+      class="mt-4 bg-black/5 p-4 rounded-lg w-70 items-center flex justify-center"
+    >
+      <ActionWithIcon :icon=" DownloadIcon" text="Download company setup" />
+    </div>
+    <div>
+      <Tab :tabs="tabs" />
+    </div>
+
+    <RouterView />
+  </div>
+</template>
+<script setup lang="ts">
+import Tab from "@/components/UI/CompanyTab/Tab.vue";
+import ActionWithIcon from "../../components/UI/Buttons/ActionWithIcon.vue";
+import DownloadIcon from "../../assets/icons/Download/DownloadIcon.vue";
+
+
+const tabs = [
+  { label: "Branches", to: "/company-setup/branches" },
+  { label: "Positions", to: "/company-setup/positions" },
+  { label: "Payout Templates", to: "/company-setup/payout-templates" },
+];
+</script>
