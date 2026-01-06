77f33316c180feeb5febee28fde67795b940269a
chhagan123 <chhaganrakhade7@gmail.com>
2026-01-06 23:39:22 +0530

    feat: add BaseTable component

---
+++ src/components/UI/Tables/BaseTable.vue
@@ -0,0 +1,92 @@
+<template>
+    <table class="min-w-full border border-white rounded-2xl">
+   <!--Header-->
+      <thead>
+        <tr class="bg-gray-100">
+          <th
+            v-for="col in columnDefs"
+            :key="col.field"
+            class="px-4 py-2 border border-black/10 rounded-xl text-left"
+          >
+            <!-- Header content -->
+            <div class="flex items-center justify-between">
+              <span>{{ col.label }}</span>
+  
+              <!-- Header Icon -->
+              <component
+                v-if="col.headerIcon"
+                :is="col.headerIcon.component"
+                v-bind="col.headerIcon.props"
+                class="w-6 h-6 cursor-pointer"
+                @click="col.headerIcon.onClick?.()"
+              />
+            </div>
+          </th>
+        </tr>
+      </thead>
+  
+      <!-- BODY -->
+      <tbody>
+        <tr
+          v-for="(row, rowIndex) in rowData"
+          :key="rowIndex"
+          class="hover:bg-gray-50 border-black/10 rounded-xl"
+        >
+          <td
+            v-for="col in columnDefs"
+            :key="col.field"
+            class="px-4 py-2 border-3 border-black/10 rounded-xl"
+          >
+            <div class="flex items-center gap-2">
+              <!-- Normal row value -->
+              <span>{{ row[col.field] }}</span>
+  
+              <!-- Row Icon(s) -->
+              <template v-if="col.cellIcon">
+                <component
+                  v-for="(icon, idx) in normalizeIcons(col.cellIcon)"
+                  :key="idx"
+                  :is="icon.component"
+                  v-bind="icon.props"
+                  class="w-6 h-6 cursor-pointer"
+                  @click="icon.onClick?.(row)"
+                />
+              </template>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </template>
+  
+  <script setup lang="ts">
+  /* TYPES*/
+  
+  interface IconConfig {
+    component: any
+    props?: Record<string, any>
+    onClick?: (row?: any) => void
+  }
+  
+  interface ColumnDef {
+    label: string
+    field: string
+    headerIcon?: IconConfig
+    cellIcon?: IconConfig | IconConfig[]
+  }
+  
+  /*PROPS */
+  
+  const props = defineProps<{
+    rowData: any[]
+    columnDefs: ColumnDef[]
+  }>()
+  
+  /*  HELPERS */
+  
+  // Always return array for row icons
+  const normalizeIcons = (icon: IconConfig | IconConfig[]) => {
+    return Array.isArray(icon) ? icon : [icon]
+  }
+  </script>
+  
