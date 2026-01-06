3fd0ba0a846ed3b7f4c4143bfb74aa56ef4a657a
chhagan123 <chhaganrakhade7@gmail.com>
2026-01-06 15:36:28 +0530

    feat: added arrow, chevron, clock, code, delete, job, plus and search icons

---
+++ src/assets/icons/Arrow/ArrowIcon.vue
@@ -0,0 +1,22 @@
+<template>
+    <svg
+      width="20"
+      height="21"
+      viewBox="0 0 20 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      class="w-5 h-5 relative"
+    >
+      <g id="Outline Icons">
+        <path
+          id="Vector"
+          d="M12.5 13.14L10.8333 17.3067L7.49999 8.14003L16.6667 11.4734L12.5 13.14ZM12.5 13.14L16.6667 17.3067M5.98999 2.50586L6.63749 4.92003M4.27999 7.27753L1.86499 6.63003M11.625 4.01503L9.85666 5.78336M5.14249 10.4967L3.37582 12.265"
+          stroke="black"
+          stroke-opacity="0.76"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></path>
+      </g>
+    </svg>
+  </template>
