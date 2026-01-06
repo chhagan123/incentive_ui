caca2e3c469a9082aed727df434bfc8ac9434f2b
chhagan123 <chhaganrakhade7@gmail.com>
2026-01-06 15:52:51 +0530

    feat: added CompanySetup Branches page

---
+++ src/views/CompanySetup/Branches.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="w-full h-full flex flex-col justify-start gap-8">
+      <div class="flex flex-col gap-1">
+        <div class="text-md font-bold text-[#000000BF]">BRANCHES</div>
+        <div>
+          Add Branches and manage branch details like Teams and Positions from
+          here
+        </div>
+      </div>
+      <div class="gap-2 flex items-center">
+        <div
+          class="bg-black/10 rounded-lg w-8 h-8 items-center justify-center flex"
+        >
+          <Arrow class="w-6 h-6" />
+        </div>
+        <div class="text-xl font-bold text-[#000000BF]">Actions</div>
+      </div>
+      <div
+        class="flex justify-between h-[166px] items-center gap-6 border-2 rounded-2xl border-black/5 p-8"
+      >
+       
+        <div class="flex gap-6 w-3/5">
+          <BaseInput
+            label="Branch Code"
+            placeholder="Enter a Branch Code"
+            required
+          />
+  
+          <BaseInput
+            label="Job Titles"
+            placeholder="Select one or more Job Titles"
+            type="select"
+            required
+          />
+        </div>
+  
+        <div
+          class="bg-[#CF6768] hover:bg-red-400 w-40 h-10 rounded-sm flex items-center justify-center text-white text-md mt-3 cursor-pointer"
+        >
+          <ActionWithIcon :icon="Plus" text="Create Branch" />
+        </div>
+      </div>
+      <div class="gap-2 flex items-center">
+        <div
+          class="bg-black/10 rounded-lg w-8 h-8 items-center justify-center flex"
+        >
+          <Arrow class="w-6 h-6" />
+        </div>
+        <div class="text-xl font-bold text-[#000000BF]">Branches</div>
+      </div>
+      <div class="flex gap-2">
+        <input
+            type="text"
+            placeholder="What are you looking for?"
+  
+            class="w-full h-[44px] rounded-lg border border-gray-200 px-4 pr-10 text-sm text-gray-700 placeholder-gray-400 cursor-pointer focus:outline-none focus:ring-1 focus:ring-[#CF6768]"
+          />
+          <div
+          class="bg-[#CF6768] w-30 h-10 rounded-sm flex items-center justify-center text-white text-md "
+        >
+          <ActionWithIcon :icon="SearchIcon" text="Search" />
+        </div>
+      </div>  
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import Arrow from "../../assets/icons/Arrow/ArrowIcon.vue";
+  import BaseInput from "../../components/UI/Inputs/BaseInput.vue";
+  import ActionWithIcon from "../../components/UI/Buttons/ActionWithIcon.vue";
+  import Plus from "../../assets/icons/Plus/PlusIcon.vue";
+  import SearchIcon from "../../assets/icons/Search/SearchIcon.vue";
+
+  
+  </script>
