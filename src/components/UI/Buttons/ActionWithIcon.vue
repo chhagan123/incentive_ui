04632cde66aab28fc267049a26406a60fffb25fc
chhagan123 <chhaganrakhade7@gmail.com>
2026-01-06 12:47:35 +0530

    feat: added ActionWithIcon button compoenent

---
+++ src/components/UI/Buttons/ActionWithIcon.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="flex items-center rounded-2xl">  
+      <component :is="icon" class="w-6 h-6 text-gray-400 mr-2" />
+      <button @click="handleClick" class="font-medium text-nowrap ">
+        {{ text }}
+      </button>
+    </div>
+  </template>
+  
+  <script setup>
+  import { defineProps, defineEmits } from "vue";
+  
+  const props = defineProps({
+    text: {
+      type: String,
+      default: "Download",
+    },
+    icon: {
+      type: [Object, Function, String],
+      default: "downloadIcon", // you can pass your icon component
+    },
+  });
+  
+  const emit = defineEmits(["click"]);
+  
+  const handleClick = () => {
+    emit("click");
+  };
+  </script>
+  
