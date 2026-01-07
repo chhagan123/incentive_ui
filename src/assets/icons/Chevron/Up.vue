fb365598610abdc51efffb18cb4f7ec3561bdff9
chhagan123 <chhaganrakhade7@gmail.com>
2026-01-07 13:48:10 +0530

    feat: added chevron up icon

---
+++ src/assets/icons/Chevron/Up.vue
@@ -0,0 +1,10 @@
+<template>
+    <svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke-width="1.5"
+  stroke="#3D3D3D"
+  class="w-6 h-6 ml-4"
+><path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5"></path></svg>
+</template>
