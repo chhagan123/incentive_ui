4d48febe3ebdeaa9a44af6d2cf72250b1d0bee34
chhagan123 <chhaganrakhade7@gmail.com>
2026-01-08 16:00:13 +0530

    feat: add RightArrow icon component

---
+++ src/assets/icons/Arrow/RightArrow.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg
+    width="18"
+    height="19"
+    viewBox="0 0 18 19"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class="w-[18px] h-[18px]"
+  >
+    <path
+      d="M10.5 4.39062L15.75 9.64062M15.75 9.64062L10.5 14.8906M15.75 9.64062H2.25"
+      stroke="white"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    ></path>
+  </svg>
+</template>
